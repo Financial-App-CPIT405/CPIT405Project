10abac1b33cab4cec097a0aa79793ff4ea2c8da9
BwShamlan <BMOHAMMEDSHAMLAN@stu.kau.edu.sa>
2023-12-08 02:19:31 +0300

    Add App files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/home";
+import BudgetManager from "./components/BudgetManager";
+import FinancialGoals from "./components/FinancialGoals";
+import CurrencyConverter from "./components/CurrencyConverter";
+import StockMarketInfo from "./components/StockMarketInfo";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <header>
+          <nav className="navbar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/budget">Budget Manager</Link>
+              </li>
+              <li>
+                <Link to="/financial-goals">Financial Goals</Link>
+              </li>
+              <li>
+                <Link to="/currency-converter">Currency Converter</Link>
+              </li>
+              <li>
+                <Link to="/stock-market">Stock Market Info</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/budget" element={<BudgetManager />} />
+            <Route path="/financial-goals" element={<FinancialGoals />} />
+            <Route path="/currency-converter" element={<CurrencyConverter />} />
+            <Route path="/stock-market" element={<StockMarketInfo />} />
+          </Routes>
+        </main>
+
+        <footer>
+          <p>Finance App (CPIT405)</p>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
