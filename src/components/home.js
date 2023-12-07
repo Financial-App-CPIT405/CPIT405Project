c513bc5a260b286df7a627f29950a53fdadf051b
Ahmad-Mohammed-Bakadam <98517205+Ahmad-Mohammed-Bakadam@users.noreply.github.com>
2023-12-08 02:26:09 +0300

    Create home.js

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Financial App!</h1>
+      <p>This app helps you track your finances, set budgets, manage your financial goals, convert currencies, and fetch stock market info.</p>
+    </div>
+  );
+};
+  
+export default Home;
